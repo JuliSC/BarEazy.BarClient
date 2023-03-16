03fee711d63eb1ff4ccbd190036691555b958cbe
Julius Stenbæk Christoffersen <julius@adnudging.com>
2023-03-16 14:28:16 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import Navigation from "./navigation/Navigation";
+
+function App() {
+  return (
+    <div
+      id="app"
+      className="relative min-h-screen flex flex-col justify-center items-center"
+    >
+      <Navigation />
+    </div>
+  );
+}
+
+export default App;
